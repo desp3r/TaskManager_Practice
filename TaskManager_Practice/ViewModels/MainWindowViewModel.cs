43e7d6f32f9698aa43a24def04c80e22bf882d4f
desp3r <sofckngeasy@gmail.com>
2021-06-07 17:32:28 +0300

    Добавление MainWindowViewModel

---
+++ TaskManager_Practice/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager_Practice.ViewModels.Base;
+
+namespace TaskManager_Practice.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+
+    }
+}
