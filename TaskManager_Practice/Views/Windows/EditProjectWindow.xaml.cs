61f479290c7c6ab84382459dc79a5c552fa7c5c9
desp3r <sofckngeasy@gmail.com>
2021-06-11 20:24:38 +0300

    файлы

---
+++ TaskManager_Practice/Views/Windows/EditProjectWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using TaskManager_Practice.EntityFramework;
+using TaskManager_Practice.Models;
+
+namespace TaskManager_Practice.Views.Windows
+{
+    public partial class EditProjectWindow : Window
+    {
+        private Project _project { get; set; }
+        
+        public EditProjectWindow(Project project)
+        {
+            _project = project;
+            InitializeComponent();
+            InfoName.Text = _project.Name;
+            DataContext = this;
+        }
+
+        private void SaveChanges(object sender, RoutedEventArgs e)
+        {
+            var db = new MyDbContext();
+            
+            db.EditProject(_project);
+            db.SaveChanges();
+            this.Close();
+        }
+    }
+}
