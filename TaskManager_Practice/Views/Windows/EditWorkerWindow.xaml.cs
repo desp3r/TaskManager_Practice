a3ff2b9eda8c2051354cecd5319990714f494fc4
desp3r <sofckngeasy@gmail.com>
2021-06-12 20:27:03 +0300

    файлы

---
+++ TaskManager_Practice/Views/Windows/EditWorkerWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace TaskManager_Practice.Views.Windows
+{
+    public partial class EditWorkerWindow : Window
+    {
+        public EditWorkerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
